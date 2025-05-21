d0aef5348f71f1ec7d5a508f3948300e53dcec44
Natalia <147085553+nxtalia@users.noreply.github.com>
2025-05-21 10:55:34 +0200

    Set up

---
+++ MasterlistOfBusiness/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MasterlistOfBusiness.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
