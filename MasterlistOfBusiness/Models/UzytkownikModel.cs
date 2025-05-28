778e628ce51fa1807ee66050e68a94f02d406e72
JaroMasta <147257065+JaroMasta@users.noreply.github.com>
2025-05-28 10:09:30 +0200

    Update UzytkownikModel.cs

---
+++ MasterlistOfBusiness/Models/UzytkownikModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MasterlistOfBusiness.Models
+{
+    public class Uzytkownik
+    {
+        [Key]
+        [Display(Name = "Login")]
+        public string login { get; set; }
+
+        [Required]
+        [Display(Name = "Haslo")]
+        public string haslo { get; set; }
+
+        [Display(Name = "Typ konta")]
+        //TODO: Zamienić na enum
+        public string? typ { get; set; }
+
+        // Nawigacja do encji Sprzedawca
+        public virtual ICollection<Sprzedawca>? Sprzedawca { get; set; } = new List<Sprzedawca>();
+    }
+}
